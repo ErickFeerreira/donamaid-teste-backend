d27ad005d3cb00dce8dbf8ef8d8b082a9592a7c2
ErickFeerreira <erickfeerreira@hotmail.com>
2020-07-20 14:15:13 -0300

    Migration: add user_id to adress

---
+++ database/migrations/2020_07_20_171258_add_user_id_to_adresses.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToAdresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('adresses', function (Blueprint $table) {
+            $table->int('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('adresses', function (Blueprint $table) {
+            //
+        });
+    }
+}
