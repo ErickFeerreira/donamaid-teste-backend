9af3e8e37246c6ce29ec5d7b3854f26cb9ef42e2
ErickFeerreira <erickfeerreira@hotmail.com>
2020-07-21 01:30:46 -0300

    Order filters

--- app/Http/Controllers/ProfessionalController.php
+++ app/Http/Controllers/ProfessionalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Professional;
+use App\Order;
 class ProfessionalController extends Controller
 {
     public function create (Request $request){
