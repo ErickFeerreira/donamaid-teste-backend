f4cdeaad2b8563f59135bba6c477ebd5d880496c
ErickFeerreira <erickfeerreira@hotmail.com>
2020-07-22 22:17:11 -0300

    ...

--- routes/api.php
+++ routes/api.php
@@ -44,3 +44,11 @@ Route::put('orders/{id}', 'OrderController@update');
 Route::get('orders/{id}', 'OrderController@read');
 Route::get('orders', 'OrderController@readAll');
 Route::get('ordersby', 'OrderController@readAndFilter');
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
+Route::get('open', 'DataController@open');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', 'UserController@getAuthenticatedUser');
+    Route::get('closed', 'DataController@closed');
+});
