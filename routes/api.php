e1441c07e0e0b85d4110284fc4e86f9b2207b599
ErickFeerreira <erickfeerreira@hotmail.com>
2020-07-22 22:46:54 -0300

    ...

--- routes/api.php
+++ routes/api.php
@@ -17,38 +17,39 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('clients', "ClientController@create");
-Route::delete('clients/{id}', "ClientController@delete");
-Route::put('clients/{id}', 'ClientController@update');
-Route::get('clients/{id}', 'ClientController@read');
-Route::get('clients', 'ClientController@readAll');
-Route::get('clients/getBy', 'ClientController@readAndFilter');
 
-Route::post('adresses', "AdressController@create");
-Route::delete('adresses/{id}', "AdressController@delete");
-Route::put('adresses/{id}', 'AdressController@update');
-Route::get('adresses/{id}', 'AdressController@read');
-Route::get('adresses', 'AdressController@readAll');
-
-Route::post('professionals', "ProfessionalController@create");
-Route::delete('professionals/{id}', "ProfessionalController@delete");
-Route::put('professionals/{id}', 'ProfessionalController@update');
-Route::get('professionals/{id}', 'ProfessionalController@read');
-Route::get('idleprofessionals', 'ProfessionalController@getIdle');
-
-Route::get('professionals', 'ProfessionalController@readAll');
-
-Route::post('orders', "OrderController@create");
-Route::delete('orders/{id}', "OrderController@delete");
-Route::put('orders/{id}', 'OrderController@update');
-Route::get('orders/{id}', 'OrderController@read');
-Route::get('orders', 'OrderController@readAll');
-Route::get('ordersby', 'OrderController@readAndFilter');
 Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@authenticate');
 Route::get('open', 'DataController@open');
 
 Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::post('clients', "ClientController@create");
+    Route::delete('clients/{id}', "ClientController@delete");
+    Route::put('clients/{id}', 'ClientController@update');
+    Route::get('clients/{id}', 'ClientController@read');
+    Route::get('clients', 'ClientController@readAll');
+    Route::get('clients/getBy', 'ClientController@readAndFilter');
+
+    Route::post('adresses', "AdressController@create");
+    Route::delete('adresses/{id}', "AdressController@delete");
+    Route::put('adresses/{id}', 'AdressController@update');
+    Route::get('adresses/{id}', 'AdressController@read');
+    Route::get('adresses', 'AdressController@readAll');
+
+    Route::post('professionals', "ProfessionalController@create");
+    Route::delete('professionals/{id}', "ProfessionalController@delete");
+    Route::put('professionals/{id}', 'ProfessionalController@update');
+    Route::get('professionals/{id}', 'ProfessionalController@read');
+    Route::get('idleprofessionals', 'ProfessionalController@getIdle');
+
+    Route::get('professionals', 'ProfessionalController@readAll');
+
+    Route::post('orders', "OrderController@create");
+    Route::delete('orders/{id}', "OrderController@delete");
+    Route::put('orders/{id}', 'OrderController@update');
+    Route::get('orders/{id}', 'OrderController@read');
+    Route::get('orders', 'OrderController@readAll');
+    Route::get('ordersby', 'OrderController@readAndFilter');
     Route::get('user', 'UserController@getAuthenticatedUser');
     Route::get('closed', 'DataController@closed');
 });
